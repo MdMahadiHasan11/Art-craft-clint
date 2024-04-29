0e4bd8bd2d7f97a66d5e6941f21ba7e3f79e64c7
MdMahadiHasan11 <hasanpustcse11@gmail.com>
2024-04-29 22:30:40 +0600

    category card created

--- src/pages/category/Category.jsx
+++ src/pages/category/Category.jsx
@@ -2,16 +2,17 @@
 
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import AllCard from "../allArtCard/AllCard";
 
 const Category = () => {
 
     const { subcategory } = useParams();
-    console.log(subcategory);
+    
 
 
     // const [products, setProducts] = useState({});
 
-    
+
 
     const [item, setItem] = useState([]);
     useEffect(() => {
@@ -21,7 +22,7 @@ const Category = () => {
             .then(data => {
                 console.log(data);
                 setItem(data);
-               
+
             })
 
     }, [subcategory])
@@ -29,9 +30,21 @@ const Category = () => {
 
     return (
         <div>
+            <div className="container mx-auto">
+                <div>
+                    <p data-aos="fade-down"
+                        data-aos-easing="ease-out-cubic"
+                        data-aos-duration="1000" className="text-3xl font-bold rounded-2xl text-center bg-yellow-700 py-8 mt-6 mb-2 text-white"><span>{subcategory}</span> Craft Items</p>
+                </div>
+                <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4 ">
+                    {
+                        item.map(craft => <AllCard key={craft._id}
+                            craft={craft}></AllCard>)
+                    }
 
-           <p>{item.length}</p> 
+                </div>
 
+            </div>
         </div>
     );
 };
