4bf83e7219b6b7f620cdb450d9039c80fe4c1394
MdMahadiHasan11 <hasanpustcse11@gmail.com>
2024-04-30 16:31:57 +0600

    home page done

--- src/layouts/Slider.jsx
+++ src/layouts/Slider.jsx
@@ -19,38 +19,41 @@ const Slider = () => {
                     { delay: 3000 }
                 }
             >
-                <SwiperSlide>
-                    <div className="relative">
-                        <img  src="https://i.ibb.co/6ZCxKYY/s111.jpg" alt="Your Image" className="w-full max-h-[550px]" />
-                        <div className="absolute inset-0 flex items-center justify-center">
-                            {/* text-[#1DD100] */}
-                            <div className='lg:w-3/4 w-5/6'>
-                                <p className=" text-cyan-700 lg:text-6xl md:text-4xl text-2xl font-extrabold flex justify-center items-center mb-4">Craft Landscape</p>
-                                <p className='text-center text-white font-bold lg:text-xl flex justify-center items-center mb-4'>Picture a serene landscape, where nature's brushstrokes paint a masterpiece of tranquility. Rolling hills stretch out like the waves of an endless sea.</p>
-                            </div>
-                        </div>
-                    </div>
-                </SwiperSlide>
+
 
                 <SwiperSlide>
                     <div className="relative">
                         <img src="https://i.ibb.co/ygZmrFz/s222.jpg" alt="Your Image" className="w-full max-h-[550px]" />
                         <div className="absolute inset-0 flex items-center justify-center">
                             <div className='lg:w-3/4 w-5/6' >
-                                <p className=" text-cyan-700 lg:text-6xl md:text-4xl text-2xl font-extrabold flex justify-center items-center mb-4"> Portrait Drawing</p>
+                                <p className=" text-yellow-600 lg:text-6xl md:text-4xl text-2xl font-extrabold flex justify-center items-center mb-4"> Portrait Drawing</p>
                                 <p className='text-center text-white font-bold lg:text-xl flex justify-center items-center mb-4'>Drawing a portrait involves capturing the likeness, character, and essence of a person's face through artistic expression. </p>
                             </div>
                         </div>
                     </div>
                 </SwiperSlide>
+                <SwiperSlide>
+                    <div className="relative">
+                        <img src="https://i.ibb.co/6ZCxKYY/s111.jpg" alt="Your Image" className="w-full max-h-[550px]" />
+                        <div className="absolute inset-0 flex items-center justify-center">
+                            {/* text-[#1DD100] */}
+                            <div className='lg:w-3/4 w-5/6'>
+                                <p className=" text-cyan-600 lg:text-6xl md:text-4xl text-2xl font-extrabold flex justify-center items-center mb-4">Craft Landscape</p>
+                                <p className='text-center text-white font-bold lg:text-xl flex justify-center items-center mb-4'>Picture a serene landscape, where nature's brushstrokes paint a masterpiece of tranquility. Rolling hills stretch out like the waves of an endless sea.</p>
+                            </div>
+                        </div>
+                    </div>
+                </SwiperSlide>
+
+
 
                 <SwiperSlide>
                     <div className="relative">
                         <img src="https://i.ibb.co/KWydTVw/s333.jpg" alt="Your Image" className="w-full max-h-[550px]" />
                         <div className="absolute inset-0 flex items-center justify-center">
                             <div className='lg:w-3/4 w-5/6'>
-                                <p className=" text-cyan-600 lg:text-6xl md:text-4xl text-2xl font-extrabold flex justify-center items-center mb-4">  Water-colour Painting</p>
-                                <p className='text-center text-white font-bold lg:text-xl flex justify-center items-center mb-4'>Elevate your lifestyle to new heights with our luxury high-rise residences. Indulge in breathtaking panoramic views and upscale amenities that cater to your every need. </p>
+                                <p className=" text-yellow-600 lg:text-6xl md:text-4xl text-2xl font-extrabold flex justify-center items-center mb-4">  Water-colour Painting</p>
+                                <p className='text-center text-white font-bold lg:text-xl flex justify-center items-center mb-4'>Watercolor painting is a versatile and expressive medium that involves using pigments suspended in a water-based solution to create art</p>
                             </div>
                         </div>
                     </div>
@@ -61,8 +64,8 @@ const Slider = () => {
                         <img src="https://i.ibb.co/ygZmrFz/s222.jpg" alt="Your Image" className="w-full max-h-[550px]" />
                         <div className="absolute inset-0 flex items-center justify-center">
                             <div className='lg:w-3/4 w-5/6 '>
-                                <p className=" text-cyan-600 lg:text-6xl md:text-4xl text-2xl font-extrabold flex justify-center items-center mb-4"> Serene Suburban Retreats</p>
-                                <p className='text-center text-white font-bold lg:text-xl flex justify-center items-center mb-4'>Escape the hustle and bustle of the city and discover the tranquility of suburban living. Nestled amidst lush greenery and scenic landscapes, our suburban retreats offer a peaceful sanctuary to call home. </p>
+                                <p className=" text-cyan-600 lg:text-6xl md:text-4xl text-2xl font-extrabold flex justify-center items-center mb-4"> Cartoon Drawing</p>
+                                <p className='text-center text-white font-bold lg:text-xl flex justify-center items-center mb-4'>Creating cartoon drawings involves simplifying complex subjects into exaggerated and often humorous representations. </p>
                             </div>
                         </div>
                     </div>
